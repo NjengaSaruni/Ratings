b87120220d15dcde41fcae56ebefd05c43e59e1c
Peter Njenga <peternjengask@gmail.com>
2019-05-05 01:18:57 +0300

    Render a list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    `<h1>Hello World</h1>>`,
+    <div>
+        <h1>Hello World</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>,
     document.getElementById('root')
 );
