d4457e842275ad047671c52b5ff8b5e3f8b48cf2
Peter Njenga <peternjengask@gmail.com>
2019-05-06 17:33:58 +0300

    Some work with props and class based

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Person(props) {
-    return (
-        <div className="person">
-           <p>{props.person.name}</p>
-            <img src={props.person.url} alt=""/>
-        </div>
-    )
+class Person extends React.Component {
+    render(props) {
+        return (
+            <div className="person">
+                <p>{props.person.name}</p>
+                <img className="image" src={props.person.url} alt=""/>
+            </div>
+        )
+    }
+
 }
 
 export default Person
